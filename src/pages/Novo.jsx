095d52ae260f93363a3fe67aa8033fc3d539ee22
Arthur Yamamoto <arthur.silva.yamamoto@gmail.com>
2023-04-18 22:15:18 -0300

    Criacao de componentes JSX

---
+++ src/pages/Novo.jsx
@@ -0,0 +1,8 @@
+import Section from "../components/Section"
+export default function Novo() {
+    return (
+        <Section titulo="Página Novo">
+            <p>Conteudo da página novo</p>
+        </Section>
+    )
+}
---
+++ src/pages/Pedidos.jsx
@@ -0,0 +1,8 @@
+import Section from "../components/Section"
+export default function Pedidos() {
+    return (
+        <Section titulo="Meus Pedidos">
+            <p>Conteudo da página meus pedidos</p>
+        </Section>
+    )
+}
